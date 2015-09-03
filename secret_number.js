456b01e5b3c590d5e77484db7e72160091c315f3
Bryan Alexander <bryanalexander@me.com>
2015-09-02 17:01:52 -1000

    completed closure exercise, passing all tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
 'use strict';
-module.exports = function() {
-  
-};
+
+module.exports = (function() {
+
+  var secretNumber = Math.floor(Math.random() * 100000);
+
+  return function() {
+
+    return secretNumber;
+
+  };
+
+});
